ee5610f48581192dce9ad5607166aeb1e0d0d423
RowanMayer <ramwarrior5678@outlook.com>
2021-01-21 18:27:16 -0500

    big changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState }from 'react';
+import './index.css';
+import {ReactComponent as BellIcon} from './icons/bell.svg';
+import {ReactComponent as ArrowIcon} from './icons/arrow.svg';
+import {ReactComponent as BoltIcon} from './icons/bolt.svg';
+import {ReactComponent as CaretIcon} from './icons/caret.svg';
+import {ReactComponent as ChrevronIcon} from './icons/chevron.svg';
+import {ReactComponent as CogIcon} from './icons/cog.svg';
+import {ReactComponent as MessengerIcon} from './icons/messenger.svg';
+import {ReactComponent as PlusIcon} from './icons/plus.svg';
+import {CSSTransition} from 'react-transition-group';
+
+{/*   
+      <NavItem icon={<ArrowIcon/>}/>
+      <NavItem icon={<BoltIcon/>}/>
+      <NavItem icon={<ChrevronIcon/>}/>
+      <NavItem icon={<CogIcon/>}/>
+      
+       */}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar>
+     <NavItem icon={<PlusIcon/>}/> 
+     <NavItem icon={<BellIcon />}/>
+     <NavItem icon={<MessengerIcon/>}/>
+    
+    <NavItem icon={<CaretIcon/>}>
+    <DropdownMenu/>
+
+
+    </NavItem>
+
+
+
+    </Navbar>
   );
 }
 
+function DropdownMenu() {
+
+  const [activeMenu, setActiveMenu] = useState('main');
+
+function DropdownItem(props) {
+  return(
+    <a href="#" className='menu-item' onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
+      <span className='icon-button'>{props.leftIcon}</span>
+      {props.children}
+      <span className='icon-right'>{props.iconRight}</span>
+    </a>
+  )
+}
+
+return (
+<div className='dropdown'>
+  
+  
+  <CSSTransition in={activeMenu === 'main'} 
+  unnmountOnExit timeout={500} className='menu-primary'>
+  
+  <div className='menu'>
+  <DropdownItem>My Profile</DropdownItem>
+  <DropdownItem
+  leftIcon={<CogIcon/>}
+  iconRight={<ChrevronIcon/>}
+  goToMenu="settings">
+    Settings
+  </DropdownItem>
+  </div>
+  </CSSTransition>
+
+  <CSSTransition in={activeMenu === 'settings'} 
+  unnmountOnExit timeout={500} className='menu-secondary'>
+  
+  <div className='menu'>
+  <DropdownItem leftIcon={<ArrowIcon/>} goToMenu="main"/>
+  <DropdownItem>Settings</DropdownItem>
+  </div>
+  </CSSTransition>
+  
+</div>
+);
+
+
+
+}
+
+
+function Navbar(props) {
+  return (
+    <nav className="navbar">
+      <ul className='navbar-nav'>
+        { props.children }
+      </ul>
+    </nav>
+  );
+}
+
+function NavItem(props) {
+
+const [open, setOpen] = useState(false);
+
+  return (
+    <li className='nav-item'>
+      <a href='#' className='icon-button' onClick={() => setOpen(!open)}>
+        {props.icon}
+      </a>
+      {open && props.children}
+    </li>
+  )
+}
 export default App;
